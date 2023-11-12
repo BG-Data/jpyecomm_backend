9fc81ba9f72625824b2c857fc33a34d23fac18ef
Leon Balloni <leon.balloni@makes.ai>
2023-11-12 16:00:29 -0300

    order by structure.

--- src/common/generic.py
+++ src/common/generic.py
@@ -26,10 +26,11 @@ class CrudService(DatabaseSessions):
     def get_itens(self,
                   kwargs: dict,
                   session: Session) -> List[BaseModel]:
+        # TODO -> refatorar para incluir cinco esquemas em um padrão de contrato front/back bem definido para: filtros, ordernação, agrupamento, limit/offset/paginação e joins
         'Recupera itens de acordo com os argumentos adicionados em dicionário'
         try:
             item = session.query(self.model)
-            if (limit := kwargs.pop('limit', None)):
+            if (limit := kwargs.pop('limit', 10)):
                 logger.info(f"limite aplicado {limit}")
                 limit = int(limit)
             if (offset := kwargs.pop('offset', 0)):
@@ -37,9 +38,10 @@ class CrudService(DatabaseSessions):
             self.model_util.check_model_kwargs(kwargs)
             kwargs = self.model_util.convert_model_attributes(kwargs)
             if kwargs:
+                sql_order_by_list, kwargs = self.model_util.order_by_conditions(kwargs)
                 sql_filters = self.model_util.filter_conditions(kwargs)
                 item = item.filter(text(sql_filters.get('filter'))).params(sql_filters.get('values'))
-            item = item.limit(limit).offset(offset)
+            item = item.limit(limit).offset(offset).order_by(*sql_order_by_list)
             return [self.base_schema.model_validate(queried) for queried in item.all()]
         except Exception as exp:
             logger.error(f'Erro at >>>>> get_itens: {exp}')
