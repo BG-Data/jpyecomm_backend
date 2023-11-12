a3d18a412c9b6f706a02e6dbfb3f1e86de91996c
Leon Balloni <leon.balloni@makes.ai>
2023-11-12 01:38:21 -0300

    infisical basic cred recovery.

--- src/main.py
+++ src/main.py
@@ -2,13 +2,15 @@ import uvicorn
 from routes import init_app
 from settings import Config
 
+config = Config()
+
 app = init_app()
 app_base_configs = {
     "host": "0.0.0.0",
-    "port": Config.PORT,
-    "workers": Config.UVICORN_WORKERS,
+    "port": config.PORT,
+    "workers": config.UVICORN_WORKERS,
     "access_log": True,
-    "reload": Config.RELOAD
+    "reload": config.RELOAD
 }
 if __name__ == '__main__':
     uvicorn.run("main:app", **app_base_configs)
